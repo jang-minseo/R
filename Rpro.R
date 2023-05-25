889b7fb2415630d7cec1a9d918f3be9a2c733923
jang-minseo <79886521+jang-minseo@users.noreply.github.com>
2023-05-25 19:57:56 +0900

    commit

--- Rpro.R
+++ Rpro.R
@@ -64,3 +64,16 @@ colnames(ds) <- c('국어','영어','수학,','물리','음악')
 ds
 
 radarchart(ds)
+
+radarchart(ds,  #데이터프레임
+           pcol = 'darkgreen', # 다각형 선의 색
+           pfcol = rgb(0.2,0.5,0.5,0.5), # 다각형 내부 색
+           plwd = 3, # 다각형 선의 두께
+           cglcol = 'grey', # 거미줄의 색
+           cglty = 1, # 거미줄의 타입
+           cglwd = 0.8, # 거미줄의 두께
+           axistype = 1, # 축의 레이블 타입
+           seg = 4, # 축의 눈금 분할
+           axislabcol = 'grey', # 축의 레이블 색
+           caxislabels = seq(0,100,25) # 축의 레이블 값
+)
